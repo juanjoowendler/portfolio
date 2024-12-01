157f61dc3a1f62a944e96f43be738251ce2bc666
Wendler Juan Jose <juanjoowendler@gmail.com>
2024-12-01 20:42:02 -0300

    changes

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -66,7 +66,6 @@ export default function Page() {
                 altText={work.company}
                 title={work.company}
                 subtitle={work.title}
-                href={work.href}
                 badges={work.badges}
                 period={`${work.start} - ${work.end ?? "Present"}`}
                 description={work.description}
